d936ce97672aced484537fa26dd5df6b0574f166
Big <sorravit.bunjongpean@refinitiv.com>
2020-10-21 19:32:05 +0700

    init

---
+++ src/main/kotlin/com/sorravit/example/kotlin_springboot_flyway/KotlinSpringbootFlywayApplication.kt
@@ -0,0 +1,11 @@
+package com.sorravit.example.kotlin_springboot_flyway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootFlywayApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringbootFlywayApplication>(*args)
+}
---
+++ src/test/kotlin/com/sorravit/example/kotlin_springboot_flyway/KotlinSpringbootFlywayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sorravit.example.kotlin_springboot_flyway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootFlywayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
